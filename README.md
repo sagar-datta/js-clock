1f4f867d6d9a4478779771922af31e585b207b23
Sagar Datta <s3605651@student.rmit.edu.au>
2019-05-31 12:16:38 +1000

    changed font

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# js-clock
+# Javascript Clock
+
+## What does this do?
+
+This application displays the current time to the user and increments it every second.
--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-
-var noon = 12;
-var lunchtime = 12;
-var evening = 18;
-
-// function to have current time on page
+// function to get the current time and display it
 var currentTime = function()
 {
-    // display the string on the webpage
+    // show clock on html
     var clock = document.getElementById('clock');
  
     var currentTime = new Date();
@@ -14,37 +9,36 @@ var currentTime = function()
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
-    var meridian = "AM";
+    var meridian = "PM";
  
-    // Set hours
-	  if (hours >= noon)
+    // hrs
+	  if (hours <= 12)
 	  {
-		  meridian = "PM";
+		  meridian = "AM";
 	  }
 
-	  if (hours > noon)
+	  if (hours > 12)
 	  {
 		  hours = hours - 12;
 	  }
  
-    // Set Minutes
+    // mins
     if (minutes < 10)
     {
         minutes = "0" + minutes;
     }
  
-    // Set Seconds
+    // seconds
     if (seconds < 10)
     {
         seconds = "0" + seconds;
     }
  
-    // put together the string that displays the time
-    var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
+    // format string
+    var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + " \nヘ( ^o^)ノ＼(^_^ )ヘ";
  
     clock.innerText = clockTime;
 };
 
 // increment clock every second
-var oneSecond = 1000;
-setInterval( currentTime, oneSecond);
+setInterval( currentTime, 1000);
